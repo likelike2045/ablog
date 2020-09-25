ada51d7540a0a18c4c9ff896c1cf766b6ca03b84
Joe Lee <joeleemail@yahoo.com>
2020-09-25 20:03:15 +0800

    create change password page/forms

--- members/urls.py
+++ members/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
 
 app_name = 'members'
 urlpatterns = [
 	path('register/',UserRegisterView.as_view(),name='register'),
 	path('edit_profile/',UserEditView.as_view(),name='edit-profile'),
-
+	# path('password/',auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+	path('password/',PasswordsChangeView.as_view(), name='change-password'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
+from django.contrib.auth.views import PasswordChangeView
 
 class UserRegisterView(generic.CreateView):
 
@@ -12,9 +13,22 @@ class UserRegisterView(generic.CreateView):
 
 class UserEditView(generic.UpdateView):
 
-	form_class = UserChangeForm
+	form_class = EditProfileForm
 	template_name = 'registration/edit_profile.html'
 	success_url = reverse_lazy('theblog:home')
 
 	def get_object(self):
 		return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+	form_class = PasswordChangeForm
+	template_name='registration/change_password.html'
+	success_url = reverse_lazy('theblog:home')
+	success_message = "Post was deleted successfully."
+
+	def change(self, request, *args, **kwargs):
+		messages.success(self.request, self.success_message)
+		return super(PasswordsChangeView, self).change(request, *args, **kwargs)
+
+
+
